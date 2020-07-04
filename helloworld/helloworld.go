39711523b7de313c0a2d5f92d0394eaac4ee9f7c
Syed Saad Ahmed <syedahmed@MacBook-Pro.fritz.box>
2020-07-04 04:20:52 +0200

    adding code for the REST API

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -1,13 +1,58 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
 
-func main() {
+type User struct {
+	Username string `json: username`
+	Password string `json: password`
+}
+
+func setupRouter() *gin.Engine {
+
 	r := gin.Default()
+
+	// Ping test
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "REST API")
+	})
+
+	// Get user value
+	r.GET("/user/:name", func(c *gin.Context) {
+		user := c.Params.ByName("name")
+		c.JSON(http.StatusOK, gin.H{"user": user})
+	})
+
+	// Get some JSON value
+	r.GET("/getJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"user": "saad",
+			"language":  "golang",
+		}
+		c.AsciiJSON(http.StatusOK, data)
+	})
+
+	//using POST method, post some JSON and get back
+	r.POST("/postdata", func(c *gin.Context) {
+		var u User
+		c.BindJSON(&u)
+		c.JSON(http.StatusOK, gin.H{
+			"user": u.Username,
+			"pass": u.Password,
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
 }
